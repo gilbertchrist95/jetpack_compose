bb5caa59f2bd0c77c0a47d14dc8d49dd408fa76e
Gilbert Christopher <gilbertchrist95@gmail.com>
2021-12-30 20:50:53 +0700

    try basic

--- app/src/main/java/com/gilbert/jetpackcompose/ui/theme/Theme.kt
+++ app/src/main/java/com/gilbert/jetpackcompose/ui/theme/Theme.kt
@@ -45,3 +45,22 @@ fun JetpackComposeTheme(
         content = content
     )
 }
+
+@Composable
+fun BasicsCodelabTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme (
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
