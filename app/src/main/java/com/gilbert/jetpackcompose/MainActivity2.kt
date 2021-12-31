07031c7802dd2081201e0f3fb4c01417dc0046a1
Gilbert Christopher <gilbertchrist95@gmail.com>
2021-12-31 13:58:38 +0700

    Lazy column

--- app/src/main/java/com/gilbert/jetpackcompose/MainActivity2.kt
+++ app/src/main/java/com/gilbert/jetpackcompose/MainActivity2.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -11,6 +13,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.gilbert.jetpackcompose.ui.theme.BasicsCodelabTheme
+
+val names: List<String> = List(1000) { "$it" }
 
 class MainActivity2 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,9 +28,13 @@ class MainActivity2 : ComponentActivity() {
 }
 
 @Composable
-private fun Greetings(names: List<String> = listOf("World", "Compose")){
-    Column {
-        names.forEach { Greeting(name = it) }
+private fun Greetings(names: List<String>){
+    LazyColumn(
+        modifier = Modifier.padding(vertical = 4.dp)
+    ) {
+        items(items = names) { name ->
+            Greeting(name = name)
+        }
     }
 }
 
@@ -89,7 +97,7 @@ fun MyApp(){
     if (shouldShowOnBoarding){
         OnboardingScreen(onContinueClick = { shouldShowOnBoarding = false })
     }else{
-        Greetings()
+        Greetings(names)
     }
 }
 
