d53b93b4647e5a673b3e0fd819a8932a4fc8ab17
Gilbert Christopher <gilbertchrist95@gmail.com>
2021-12-31 14:44:11 +0700

    animating list

--- app/src/main/java/com/gilbert/jetpackcompose/MainActivity2.kt
+++ app/src/main/java/com/gilbert/jetpackcompose/MainActivity2.kt
@@ -3,11 +3,15 @@ package com.gilbert.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,7 +46,13 @@ private fun Greetings(names: List<String>){
 fun Greeting(name: String) {
     val expanded = remember { mutableStateOf(false) }
 
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(
+        if (expanded.value) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Surface(
         color = MaterialTheme.colors.primary,
@@ -52,7 +62,7 @@ fun Greeting(name: String) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding),
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp)),
             ) {
                 Text(text = "Hello, ")
                 Text(text = name)
@@ -69,8 +79,6 @@ fun Greeting(name: String) {
 
 @Composable
 fun OnboardingScreen(onContinueClick: () -> Unit){
-    var shouldShowOnBoarding by remember { mutableStateOf(true) }
-
     Surface {
         Column(
             modifier = Modifier.fillMaxSize(),
@@ -90,7 +98,7 @@ fun OnboardingScreen(onContinueClick: () -> Unit){
 
 @Composable
 fun MyApp(){
-    var shouldShowOnBoarding by remember {
+    var shouldShowOnBoarding by rememberSaveable {
         mutableStateOf(true)
     }
 
